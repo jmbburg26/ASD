bd7fe794cdb9eef71e4a21eff881e811e8245cba
John Brandenburg <jmbburg26@fullsail.edu>
2013-04-16 19:32:29 -0400

    Re-set repo
    Re-added files to the repo due to client issues.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// John Brandenburg
+// Term: 1304
+
+$("#add footer a").on("click", function(){
+	//When user clicks in the nav bar
+	//they will be taken to the add assignment
+	//page
+});
+
+$("#form").on("pageinit", function(){
+	//when the form loads, the user will be prompted
+	//to add their data to the form and submit
+});
