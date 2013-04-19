8a1826fab3a1eb6313257f48e4488fe3dbfc0e9d
John Brandenburg <jmbburg26@fullsail.edu>
2013-04-18 22:23:55 -0400

    Added the store data function and view assignments page
    The data will store to local storage but will show as an object.
    Displays the values on the View page.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,18 @@ $('#home').on('pageinit', function(){
 
 });	
 
-$('form').submit(function() {
-  console.log($(this).serializeArray());
-  return false;
+$('#add').on('pageinit', function(){	
+	
+	//Collect data from form
+	$('form').submit(function() {
+		var userValues		=Math.floor(Math.random()*100000001);
+	  	var userData = $(":input").serializeArray();
+    		jQuery.each(userData, function(i, field){
+      		$("#view").append(field.value + " ");
+  
+    	});
+    	var userItems = jQuery.makeArray(userData);
+	  	localStorage.setItem(userValues, userItems);
+	  	alert("Homework Added");
+	});
 });
