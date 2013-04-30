0a05f7f7fec7d36c785a17b7caceecea6e5eea9a
John Brandenburg <jmbburg26@fullsail.edu>
2013-04-29 22:02:21 -0400

    Revert "Merge branch 'gh-pages'"
    This reverts commit 00b10013f5fae3d1bc247cf8b99c4163ff5dd058, reversing
    changes made to 4c6419222484bda8aeb5638ef689931967e1bd07.

--- js/main.js
+++ js/main.js
@@ -3,26 +3,6 @@ $('#home').on('pageinit', function(){
 
 });	
 
-var getData = function(){
-  if(localStorage.length === 0){
-      alert("There are no assignments in Local Storage so default data has been added.");
-      defaultData();
-    }else{
-  for (var i=0, j=localStorage.length; i<j; i++) {
-              var assign = localStorage[i];
-              $(''+
-                  '<div class="assignments">'+
-                  '<h2>'+ assign.datedue +'</h2>'+
-                  '<p>'+ assign.fname +'</p>'+
-                  '<p>'+ assign.lname +'</p>'+
-                  '<p>'+ assign.email +'</p>'+
-                  '<p>'+ assign.notes +'</p>'+
-                  '</div>'
-                ).appendTo('#assignments');    
-            };
-    }
-};
-
 $('#add').on('pageinit', function(){	
 	
 	//Collect data from form
@@ -33,44 +13,48 @@ $('#add').on('pageinit', function(){
       		$("#view").append(field.value + " ");
   
     	});
-
     	var userItems = jQuery.makeArray(userData);
 	  	localStorage.setItem(userValues, userItems);
 	  	alert("Homework Added");
 	});
-  //Button links
-    var displayLink = $('#view');
-    displayLink.on("click", getData);
-    //var clearLink = $('boom');
-    //clearLink.on("click", boomData);
-    //var save = $('save');
-    //save.on("click", validate);
 });
 
 $('#view').on('pageinit', function(){
-  	
+	$.getJSON('js/json.js', function(data) {
+  	var items = [];
+ 
+  	$.each(data, function(key, val) {
+    	items.push('<li id="' + key + '">' + val + '</li>');
+  	});
+ 
+  	$('<ul/>', {
+    	'class': 'my-new-list',
+   	 html: items.join('')
+  	}).appendTo('body');
+	});
+});
 
-   /* var defaultData = $(function(){
 
-    $('#assignments').empty();
-    $.ajax({
-        url: 'xhr/data.php',
-        type: 'GET',
-        dataType: 'jsonp',
-          success: function(response){
-            for (var i=0, j=response.assignments.length; i<j; i++) {
-              var assign = response.assignments[i];
-              $(''+
-                  '<div class="assignments">'+
-                  '<h2>'+ assign.datedue +'</h2>'+
-                  '<p>'+ assign.fname +'</p>'+
-                  '<p>'+ assign.lname +'</p>'+
-                  '<p>'+ assign.email +'</p>'+
-                  '<p>'+ assign.notes +'</p>'+
-                  '</div>'
-                ).appendTo('#assignments');    
-            };
-          }
-    })
-  });*/
+$(function(){
+
+  $('#assignments').empty();
+  $.ajax({
+      url: 'xhr/data.php'
+      type: 'GET',
+      dataType: 'json',
+      success: function(response){
+        for (var i=0 j=response.assignments.length; i<j; i++) {
+          var assign = response.assignments[i];
+          $(''+
+              '<div class="assignments">'+
+              '<h2>'+ assign.datedue +'</h2>'+
+              '<p>'+ assign.fname +'</p>'+
+              '<p>'+ assign.lname +'</p>'+
+              '<p>'+ assign.email +'</p>'+
+              '<p>'+ assign.notes +'</p>'+
+              '</div>'
+              ).appendTo('#assignments')
+        };
+      }
+  })
 });
